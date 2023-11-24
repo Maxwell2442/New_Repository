515bdfb24883ba47c0b12b4a5c0cd6bd08e04f57
Maksym Shnitsar <sch.maks03@gmail.com>
2023-11-24 23:25:27 +0100

    C1

--- lab_2/base_python.py
+++ lab_2/base_python.py
@@ -31,20 +31,17 @@ print("Ім'я: Максим", True)
 print("Прізвище: Лозинський", False)
 
 # None: Спеціальний об'єкт, що вказує на відсутність значення або "нічого":
-my_variable = None
-print(my_variable, end='\n\n')
+print("Вік:", None, end='\n\n')
 
 print("Завдання ііі: Вбудовані функції у Python: ")
 
 numbers = [1, 2, 3, 4, 5]
 
 # Сума sum(): Повертає суму чисел у списку:
-sum_of_numbers = sum(numbers)
-print("Сумою списку numbers є", sum_of_numbers)
+print("Сумою списку numbers є", sum(numbers))
 
 # Довжина len(): Повертає довжину об'єкта, наприклад, кількість елементів у списку чи символів у рядку:
-length_of_numbers = len(numbers)
-print("Довжиною списку numbers є", length_of_numbers)
+print("Довжиною списку numbers є", len(numbers))
 
 # Мінімальний елемент min(): Повертає найменше число ітерації
 min_of_numbers = min(numbers)
@@ -70,3 +67,55 @@ cars = ["Volvo", "Aston Martin", "Koenigsegg"]
 print("Цикл for по списку автомобілів, з пропусками рядків:", end="\n\n")
 for car in cars:
     print(car, end="\n\n")
+
+print("Завдання v: Розгалуження у Python: ")
+
+age = 18
+
+print("Перевірка на повноліття -> ", end="")
+if age >= 18:
+    print("Ви повнолітній")
+else:
+    print("Ви неповнолітній")
+
+# Розгалуження if-elif-else
+note = int(input("Введіть Вашу оцінку: ")) 
+
+if note > 89:
+    print("Відмінно")
+elif note > 69:
+    print("Добре")
+elif note > 49:
+    print("Задовільно")
+else:
+    print("Не задовільно")
+
+print("\n\nЗавдання vi: Конструкція try->except->finally у Python: ")
+
+try:
+    # Введення користувача
+    num_str = input("Введіть число: ")
+
+    # Конвертація рядка у ціле число
+    num = int(num_str)
+
+    # Ділення на 0, що призведе до помилки
+    result = 10 / num
+
+except ValueError as ve:
+    # Виловлюємо помилку, якщо введено не число
+    print(f"Помилка: {ve}. Будь ласка, введіть коректне число.")
+
+except ZeroDivisionError:
+    # Виловлюємо помилку, якщо введено 0
+    print("Помилка: Ділення на 0 не допустиме.")
+
+finally:
+    # Код, який виконається завжди, незалежно від наявності помилок
+    print("Цей блок завжди виконується, навіть якщо виникає помилка чи ні.")
+
+print("Завдання vii: Контекст-менеджер with у Python: ")
+
+print("Додаємо текст до існуючого файлу за допомогою with: ")
+with open("README.md", "a") as file:
+    file.write("\nЦе буде додано до існуючого вмісту.")
